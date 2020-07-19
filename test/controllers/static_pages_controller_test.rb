59cf487838493889d32d7e6738a4668bd7908a4a
Rick Miro <sanunes.ricardo@gmail.com>
2020-07-19 23:04:25 +0000

    STATIC | Added dynamic title handling

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Kinnect Careers"
+  end
+
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact"
   end
 
   test "should get faq" do
     get static_pages_faq_url
     assert_response :success
+    assert_select "title", "FAQ"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About"
   end
 
 end
